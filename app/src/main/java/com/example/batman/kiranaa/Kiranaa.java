0dad09e4ac4000752181834e61c708f1debb123f
darshan1504 <djethwa2810@gmail.com>
2018-02-13 00:35:11 -0800

    Connected Firebase

---
+++ app/src/main/java/com/example/batman/kiranaa/Kiranaa.java
@@ -0,0 +1,13 @@
+/* this is the file to make a connection to the firebase*/
+package com.example.batman.kiranaa;
+
+import android.app.Application;
+
+public class Kiranaa extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
